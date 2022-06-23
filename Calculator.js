22de087208530a5567766feeb0b8fb6c184ec23f
TONYCHIDI <naccyworld@gmail.com>
2022-06-23 11:56:55 +0100

    show of work in the display

--- Calculator.js
+++ Calculator.js
@@ -10,22 +10,22 @@ if (operation == "+") {
     let num1 = prompt("Enter first number here: ", "");
     let num2 = prompt("Enter second number here: \n" + num1 + " +", "");
     let sum = +num1 + +num2;
-    alert("The answer is " + sum + ". Congrats!");
+    alert("The answer is " + num1 + " + " + num2 + " = " + sum + ". Congrats!");
 } else if (operation == "-") {
     let nums1 = prompt("Enter first number here: ", "");
     let nums2 = prompt("Enter second number here: \n" + nums1 + " -", "");
     let subtract = +nums1 - +nums2;
-    alert("The answer is " + subtract + ". Congrats!");
+    alert("The answer is " + nums1 + " - " + nums2 + " = " + subtract + ". Congrats!");
 } else if (operation == "*") {
     let num3 = prompt("Enter first number here: ", "");
     let num4 = prompt("Enter second number here: \n" + num3 + " x", "");
     let multiple = +num3 * +num4;
-    alert("The answer is " + multiple + ". Congrats!");
+    alert("The answer is " + num3 + " x " + num4 + " = " + multiple + ". Congrats!");
 } else if (operation == "/") {
     let nums3 = prompt("Enter first number here: ", "");
     let nums4 = prompt("Enter second number here: \n" + nums3 + " /", "");
     let division = +nums3 / +nums4;
-    alert("The answer is " + division + ". Congrats!");
+    alert("The answer is " + nums3 + " / " + nums4 + " = " + division + ". Congrats!");
 } else if (operation == null || operation == "" || operation !== typeof Number) {
     alert("Invalid operator selected. Start afresh!");
 }else{
